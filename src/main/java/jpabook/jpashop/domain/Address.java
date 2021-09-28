16163cc5524f7a26f4d4d1000889b4f022f6e000
hajs <dev.hajs@gmail.com>
2021-09-28 17:51:23 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,12 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() { // jpa 스펙에서는 protected 까지 허용 (public 은 너무 열려 있다)
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
